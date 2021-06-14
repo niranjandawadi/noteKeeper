aeaf610025b4b0feb8db4a6291a3c499af855daa
Niranjan Dawadi <niranjan.dawadi@ebpearls.com>
2021-06-14 21:27:16 +0545

    minor file changes

---
+++ app/src/main/java/com/niranjan/android/notekeeper/NoteListAcitivity.kt
@@ -0,0 +1,32 @@
+package com.niranjan.android.notekeeper
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.niranjan.android.notekeeper.databinding.ActivityNoteListBinding
+import com.niranjan.android.notekeeper.databinding.ContentNoteListBinding
+
+class NoteListAcitivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityNoteListBinding
+    private lateinit var noteListContent : ContentNoteListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNoteListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        noteListContent = ContentNoteListBinding.inflate(layoutInflater)
+        setContentView(noteListContent.root)
+
+        setSupportActionBar(binding.toolbar)
+
+    }
+
+}
