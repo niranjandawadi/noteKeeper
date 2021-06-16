2d5af2e1dadc0be24fe831bb6f7b8b1309b7e215
niranjandawadi <niranjan.dawadi54@gmail.com>
2021-06-16 20:19:13 +0545

    normal fixes

--- app/src/main/java/com/niranjan/android/notekeeper/MainActivity.kt
+++ app/src/main/java/com/niranjan/android/notekeeper/MainActivity.kt
@@ -32,7 +32,5 @@ class MainActivity : AppCompatActivity() {
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         contentViewBinding.spinnerCourses.adapter = adapterCourses
-
-        //added new branch
     }
 }
